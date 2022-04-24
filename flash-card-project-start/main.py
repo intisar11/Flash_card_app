8e75f12d76cc7877cd8dd50b80f9ce8d6b7791cf
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-04-24 12:46:11 -0700

    intial stage

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,6 +1,22 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pd.read_csv("../flash-card-project-start/data/french_words.csv")
+
+df = pd.DataFrame(data)
+
+dict_data = df.to_dict(orient="records")
+
+
+def next_card():
+    card = random.choice(dict_data)
+    canvas.itemconfig(card_title, text= "French")
+    canvas.itemconfig(card_word, text=card["French"])
+
+
 
 
 window = Tk()
@@ -10,7 +26,7 @@ window.config(padx=50, pady=50, bg= BACKGROUND_COLOR)
 
 card_front = PhotoImage(file="./images/card_front.png")
 card_back = PhotoImage(file="./images/card_back.png")
-right_button = PhotoImage(file="./images/right.png")
+right_button = PhotoImage(file="images/right.png")
 wrong_button = PhotoImage(file="./images/wrong.png")
 
 
@@ -23,16 +39,20 @@ canvas = Canvas(width= 800, height=526)
 canvas.create_image(400, 263, image=card_front)
 canvas.grid(column=1, row=1,columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 40, "italic"))
 
 
 
-right_button = Button(image=right_button)
-right_button.grid(column= 1, row=2)
+button_1 = Button(image=right_button, highlightthickness=0, command=next_card)
+button_1.grid(column=2, row=2)
 
-wrong_button = Button(image=wrong_button)
-wrong_button.grid(column= 2, row=2)
+button_2 = Button(image=wrong_button, highlightthickness= 0)
+button_2.grid(column=1, row=2)
+
+next_card()
+
+
 
 
 
